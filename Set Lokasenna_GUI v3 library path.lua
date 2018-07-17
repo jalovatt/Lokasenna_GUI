fdbfb0ab0fa0301d7ed2cd63871edfc5154b2cb5
Lokasenna <jalovatt@hotmail.com>
2018-07-17 16:17:28 -0600

    Revert the repo to v2 (see last commit msg)

--- Set Lokasenna_GUI v3 library path.lua
+++
@@ -1,4 +0,0 @@
-local info = debug.getinfo(1,'S');
-local script_path = info.source:match[[^@?(.*[\/])[^\/]-$]]
-
-reaper.SetExtState("Lokasenna_GUI", "lib_path_v3", script_path .. "Library/", true)
