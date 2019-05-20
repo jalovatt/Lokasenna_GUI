0ce2ffb711353d243dd935e3ba57a72e9007a392
Adam Lovatt <jalovatt@hotmail.com>
2019-05-20 09:37:57 -0600

    Add ReaPack metadata

---
+++ Lokasenna_GUI library v2.lua
@@ -0,0 +1,26 @@
+--[[
+Description: Lokasenna's GUI library v2 for Lua
+Version: 2.16.3
+Changelog:
+    Fix: Scripts created with GUI Builder weren't populating their menuboxes
+    with options
+Author: Lokasenna
+Provides:
+    [main]   Lokasenna_GUI v2/Library/Set Lokasenna_GUI v2 library path.lua
+    [nomain] Lokasenna_GUI v2/Library/{Core,Classes/*,Modules/*}.lua
+Metapackage: true
+Donation: https://paypal.me/Lokasenna
+Links:
+    Forum Thread https://forum.cockos.com/showthread.php?t=177772
+    Lokasenna's Website http://forum.cockos.com/member.php?u=10417
+About:
+    Provides a framework allowing Lua scripts to use a graphical interface, since Reaper
+    has no ability to do so natively.
+
+    INSTALLATION: After installing this package, you must tell Reaper where to find the
+    library. In the Action List, find and run:
+
+    "Script: Set Lokasenna_GUI v2 library path.lua"
+--]]
+
+-- Licensed under the GNU GPL v3
